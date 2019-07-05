afd6bd740a0482d54cf25b2ecf1e43b5d4e6dc51
lbenvenuto91 <lorenzo.benvenuto@gter.it>
2019-07-05 15:52:39 +0200

    definita in maniera apparentemente corretta parte per riconoscere il sat id

--- ReadRaw.py
+++ ReadRaw.py
@@ -8,7 +8,7 @@ import sys
 
 sentence=[]
 
-with open("4_luglio_GAL_E5a/RawMeasurementsLogger_RawLog_2019_07_04_06_38_22.txt", "r") as data_file:
+with open("../4_luglio_GAL_E5a/RawMeasurementsLogger_RawLog_2019_07_04_06_38_22.txt", "r") as data_file:
     data_file_lines = data_file.readlines()
 
 #skip header
@@ -20,18 +20,7 @@ for line in no_header_file:
     record = clean_line.split(',')
     data.append(record)
 
-Svid = []
-print(data[0][0])
-print(len(data))
-for i in range(len(data)):
-    if data[i][0] == "Fix":
-        continue
-    else:
-        if data[i][11] not in Svid:
-            Svid.append(data[i][11])
-        else:
-            continue
-print(Svid)
+
 #### Constellation Type ####
 # GPS = 1
 # GLONASS = 3
@@ -41,37 +30,64 @@ print(Svid)
 # SBAS = 2
 # UNKNOWN = 9
 
-ConstellationType=[]
+Svid = []
+GPS=[]
+SBAS=[]
+GLONASS=[]
+BEIDOU=[]
+GALILEO=[]
+QZSS=[]
+UNKNOWN=[]
+#print(data[0][0])
+#print(len(data))
+
 for i in range(len(data)):
     if data[i][0] == "Fix":
         continue
-    else:
-        if data[i][25] not in ConstellationType:
-            ConstellationType.append(data[i][25])
+    elif data[i][25]=='1':
+        if data[i][11] not in GPS:
+            GPS.append(data[i][11])
         else:
             continue
+    elif data[i][25]=='2':
+        if data[i][11] not in SBAS:
+            SBAS.append(data[i][11])
+        else:
+            continue
+    elif data[i][25]=='3':
+        if data[i][11] not in GLONASS:
+            GLONASS.append(data[i][11])
+        else:
+            continue
+    elif data[i][25]=='4':
+        if data[i][11] not in QZSS:
+            QZSS.append(data[i][11])
+        else:
+            continue
+    elif data[i][25]=='5':
+        if data[i][11] not in BEIDOU:
+            BEIDOU.append(data[i][11])
+        else:
+            continue
+    elif data[i][25]=='6':
+        if data[i][11] not in GALILEO:
+            GALILEO.append(data[i][11])
+        else:
+            continue
+    elif data[i][25]=='9':
+        if data[i][11] not in UNKNOWN:
+            UNKNOWN.append(data[i][11])
+        else:
+            continue    
 
-#
-ReceivedConst=[]
-for i in ConstellationType:
-    if i == '1':
-        ReceivedConst.append('GPS')
-    elif i == '2':
-        ReceivedConst.append('SBAS')
-    elif i == '3':
-        ReceivedConst.append('GLONASS')
-    elif i == '4':
-        ReceivedConst.append('QZSS')
-    elif i == '5':
-        ReceivedConst.append('BEIDOU')
-    elif i == '6':
-        ReceivedConst.append('GALILEO')
-    else:
-        ReceivedConst.append('UNKNOWN')
+print(Svid)
+print("GPS",GPS)
+print("SBAS",SBAS)
+print("GLONASS",GLONASS)
+print("QZSS",QZSS)
+print("BEIDOU",BEIDOU)
+print("GALILEO",GALILEO)
 
-
-print(ConstellationType)
-print(ReceivedConst)
 
 sys.exit()
 '''
